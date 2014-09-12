8caddcf537c2b835e4d568dcc227b3fc7f562df0
hoanghh <hoanghh.vnu@gmail.com>
2014-09-12 14:02:46 +0700

    modified to explicit understand role of router and frontname

--- app/code/local/SM/Slider/Block/Slider.php
+++ app/code/local/SM/Slider/Block/Slider.php
@@ -1,5 +1,6 @@
 <?php
 class SM_Slider_Block_Slider extends Mage_Core_Block_Template
+//class SM_Slider_Block_Slider extends Mage_Page_Block_Html_Head
 {
     public function __construct(){
 //        echo __METHOD__;
@@ -7,59 +8,20 @@ class SM_Slider_Block_Slider extends Mage_Core_Block_Template
     }
 	public function _prepareLayout()
     {
+//        echo __METHOD__;
+        $SliderStatus = Mage::getStoreConfig('sm_slider/sm_slider/show');
+//        var_dump($SliderStatus);
+        if($SliderStatus == 1){
+//            $this->getLayout()->getBlock('head')->addItem('skin_css', 'css/idangerous.swiper.css');
+            Mage::app()->getLayout()->getBlock('head')->addItem('skin_css', 'css/idangerous.swiper.css');
+            $this->getLayout()->getBlock('head')->addItem('skin_js', 'js/idangerous.swiper.js');
+        }
+
 		return parent::_prepareLayout();
     } // end _prepareLayout()
 
 
-    public function addItem( $type, $name){
-        $name = trim($name);
-        if($name == ''){
-            return FALSE;
-        }
-//        echo "<pre>";
-//        var_dump($name);
-        if(gettype($name) == 'array'){
-            $this->addMultiItem($type, $name);
-        } else if (gettype($name) == 'string'){
-            $this->addSingleItem($type, $name);
-        } // end if gettype
-//        die();
-    } // end method addSkinJs()
 
-    public function addSingleItem($type = NULL, $name = NULL){
-        if($name == NULL){
-            return FALSE;
-        }
-        $ItemBlock = Mage::app()->getLayout()
-//            ->getBlock('head')
-//            ->createBlock('Mage_Page_Block_Html_Head')
-//            ->addItem($type,$name)
-            ;
-        var_dump($ItemBlock);
-        die();
-        $this->getLayout()
-            ->getBlock('head')
-            ->append($ItemBlock);
-    } // end method addSingleSkinItem
-
-    public function addMultiItem($type = NULL, $name = array()){
-        if( empty($name)){
-            return FALSE;
-        }
-        $ItemBlock = Mage::app()->getLayout()
-//            ->getBlock('head')
-            ->createBlock('Mage_Page_Block_Html_Head')
-        ;
-        foreach ($name as $item){
-            if($item != ''){
-                $ItemBlock->addItem($type,$item);
-            }
-        } // end foreach
-        ;
-        $this->getLayout()
-            ->getBlock('head')
-            ->append($ItemBlock);
-    } // end method addMultiSkinItem
 
     /**
      * get Array image for slider will be show
@@ -106,5 +68,66 @@ class SM_Slider_Block_Slider extends Mage_Core_Block_Template
         return $SortedImageArray;
 //        return $ImageArray;
     } // end method getSliderInfo
+
+
 } // end class
+
+/**
+ * some method to add js but it's still not work
+ */
+//    public function myaddItem( $type, $name){
+//        $name = trim($name);
+//        if($name == ''){
+//            return FALSE;
+//        }
+////        echo "<pre>";
+////        var_dump($name);
+//        if(gettype($name) == 'array'){
+//            $this->addMultiItem($type, $name);
+//        } else if (gettype($name) == 'string'){
+//            $this->addSingleItem($type, $name);
+//        } // end if gettype
+////        die();
+//    } // end method addSkinJs()
+//
+//    public function addSingleItem($type = NULL, $name = NULL){
+//        echo __METHOD__;
+//        if($name == NULL){
+//            return FALSE;
+//        }
+////        $ItemBlock = Mage::app()->getLayout()
+//        $this->getLayout()
+//            ->getBlock('head')
+////            ->createBlock('Mage_Page_Block_Html_Head')
+////            ->addItem($type,$name)
+//            ->addItem('skin_css', 'css/idangerous.swiper.css');
+//        ;
+////        var_dump($ItemBlock);
+////        die();
+////        $this->getLayout()
+////            ->getBlock('head')
+////            ->append($ItemBlock);
+//    } // end method addSingleSkinItem
+//
+//    public function addMultiItem($type = NULL, $name = array()){
+//        if( empty($name)){
+//            return FALSE;
+//        }
+//        $ItemBlock = Mage::app()->getLayout()
+//            ->getBlock('head')
+////            ->createBlock('Mage_Page_Block_Html_Head')
+//        ;
+//        foreach ($name as $item){
+//            if($item != ''){
+//                $ItemBlock->addItem($type,$item);
+//            }
+//        } // end foreach
+//        ;
+//        $this->getLayout()
+//            ->getBlock('head')
+//            ->append($ItemBlock);
+//    } // end method addMultiSkinItem
+
+
+
 // end file
